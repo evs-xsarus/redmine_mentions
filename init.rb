ba3e35a5e9db32cf75ad6635a4ee9d9bb6d889c9
Ernst Vaarties <e.vaarties@xsarus.nl>
2022-11-11 09:22:13 +0000

    Ticket #145752: remove adding mentions, is now native Redmine.

--- init.rb
+++ init.rb
@@ -1,16 +1,15 @@
 require 'redmine'
 
 Rails.configuration.to_prepare do
-  require_dependency 'redmine_mentions/hooks'
   require_dependency 'journal'
   Journal.send(:include, RedmineMentions::JournalPatch)
 end
 Redmine::Plugin.register :redmine_mentions do
   name 'Redmine Mentions'
-  author 'Arkhitech'
-  description 'This is a plugin for Redmine which gives suggestions on using username in comments'
-  version '0.0.1'
-  url 'https://github.com/arkhitech/redmine_mentions'
-  author_url 'http://www.arkhitech.com/'
+  author 'XSARUS, original by Arkhitech'
+  description 'This is a plugin for Redmine which sends mail to mentioned users in comments.'
+  version '0.0.2'
+  url 'https://github.com/evs-xsarus/redmine_mentions'
+  author_url 'http://www.xsarus.nl/'
   settings :default => {'trigger' => '@'}, :partial => 'settings/mention'
 end
